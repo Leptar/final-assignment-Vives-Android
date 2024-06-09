5478e6889b6101a6f7dcf959d076582a92e6a22c
Leptar <leo.ultimate34@gmail.com>
2024-06-09 14:48:58 +0200

    AppNotLaunchWhenNotConnected function finish

--- PokemonTCGManager/app/src/main/java/be/leocheikhboukal/pokemontcgmanager/MainActivity.kt
+++ PokemonTCGManager/app/src/main/java/be/leocheikhboukal/pokemontcgmanager/MainActivity.kt
@@ -1,22 +1,26 @@
 package be.leocheikhboukal.pokemontcgmanager
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import be.leocheikhboukal.pokemontcgmanager.ui.theme.PokemonTCGManagerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        if (!isOnline(this)) {
+            Toast.makeText(this, "No Internet connexion, the application will close", Toast.LENGTH_LONG).show()
+            finish()
+        }
         setContent {
             PokemonTCGManagerTheme {
                 // A surface container using the 'background' color from the theme
@@ -30,10 +34,16 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+/**
+ * Check if the device is connected to the internet, Ethernet, Wifi or Cellular
+ * @param context The context of the app
+ * @return True if the device is connected to the internet, false otherwise
+ */
+fun isOnline(context: Context): Boolean {
+    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+    return capabilities != null &&
+            (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET))
 }
